7bb700709db6231342308123731dd8fedb759f22
mzkarla <714875@student.dusd.net>
2023-02-16 19:33:57 -0800

    finished
    finished

--- PlayerControlP2Lopez/Assets/Scripts/PlayerControl.cs
+++ PlayerControlP2Lopez/Assets/Scripts/PlayerControl.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-   public float speed = 5.0f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   //Private Variables
+   private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+   private float horizontalInput;
+   private float forwardInput;
 
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vechicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime* speed);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on the vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime* speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
